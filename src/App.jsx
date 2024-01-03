acb3633aa3ef55be0368ffafadbbd4bc03e176a4
I Putu Julianta <thujuli037@gmail.com>
2024-01-03 23:41:59 +0800

    feat: handle minus and add on button keranjang

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+import { useState } from "react";
+import { Products } from "./data/product";
+import ProductCard from "./components/ProductCard";
+
 function App() {
-  return <h1 className="text-3xl underline text-red-600">Hello World</h1>;
+  const [products, setProducts] = useState(Products);
+
+  return (
+    <div className="max-w-[1500px] mx-auto">
+      <h1 className="text-center text-4xl font-medium mt-5">Belanja Mobil</h1>
+      <div className="grid grid-cols-4 gap-5 mt-5 p-3">
+        {products.map((product) => (
+          <ProductCard product={product} key={product.id} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
